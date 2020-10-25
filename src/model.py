8e0a3b4d9747a7fd6ca8588bee9a5ccf69cc9518
Armin Karamzade <karamzade.armin@gmail.com>
2020-10-25 15:02:26 +0330

    multinomial negative values fixed.

--- src/model.py
+++ src/model.py
@@ -113,7 +113,7 @@ class Model(nn.Module):
             output, (hn, cn), on, attn_weight = self.decoder_step(next_token, hn, cn, on, encoder_outputs)
             logits[:, t, :] = output
             if teacher_forcing_ratio < torch.rand(1).item():
-                next_token = output.multinomial(1).squeeze(1) # (batch)
+                next_token = output.exp().multinomial(1).squeeze(1) # (batch)
             else:
                 next_token = y[:, t] # (batch)
         return logits
